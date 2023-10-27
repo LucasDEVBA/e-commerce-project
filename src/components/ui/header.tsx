7de20626da84177faca1d44e82c6ad6ff7d2544d
Lucas <lucasdevba@gmail.com>
2023-10-27 13:56:13 -0300

    "feat: add header menu using sheet component"

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,15 +1,50 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  MenuIcon,
+  ShoppingCartIcon,
+  LogInIcon,
+  PercentIcon,
+  ListOrderedIcon,
+  HomeIcon,
+} from "lucide-react";
 
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button size={"icon"} variant={"outline"}>
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size={"icon"} variant={"outline"}>
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={"left"}>
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          <div className="mt-2 space-y-4">
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <LogInIcon size={16} />
+              Fazer login
+            </Button>
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <HomeIcon size={16} />
+              Início
+            </Button>
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <Image alt="DUTTY FREE" src={"./logo-dfa.svg"} width={120} height={120} />
 
