d23b0bd3fe12e24112581694f82d1293727b3f73
Lucas <lucasdevba@gmail.com>
2023-11-04 15:08:15 -0300

    feat: add product items and product components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,28 @@
-"use client";
-
 import Image from "next/image";
+import ProductList from "./components/product-list";
+import { prismaClient } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gte: 0,
+      },
+    },
+  });
   return (
-    <div className="p-5">
+    <div>
       <Image
         src={"/Top-Banner01.png"}
         width={0}
         height={0}
-        className="h-auto w-full"
+        className="h-auto w-full p-5"
         sizes="100vw"
         alt="Banner Inicial"
       />
+      <div className="mt-8">
+        <ProductList products={deals} />
+      </div>
     </div>
   );
 }
