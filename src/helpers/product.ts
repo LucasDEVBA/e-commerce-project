e4dace76c30adf35998e9222394f97eda4c21627
Lucas <lucasdevba@gmail.com>
2023-11-04 15:27:26 -0300

    fix: ensure correct product total calculater

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -11,12 +11,12 @@ const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
       totalPrice: Number(product.basePrice),
     };
   }
-  const totalPrice =
+  const totalDiscount =
     Number(product.basePrice) * (product.discountPercentage / 100);
 
   return {
     ...product,
-    totalPrice,
+    totalPrice: Number(product.basePrice) - totalDiscount,
   };
 };
 
