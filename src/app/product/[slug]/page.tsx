a598697d90b96b598e57e9f9bfdab11c5bc66cd0
Lucas <lucasdevba@gmail.com>
2023-11-12 00:15:38 -0300

    feat: add Product Images component

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { prismaClient } from "@/lib/prisma";
+import ProductImages from "./components/product-images";
 
 interface ProductDetailsPage {
   params: {
@@ -13,7 +14,11 @@ const ProductDetailsPage = async ({ params: { slug } }: ProductDetailsPage) => {
     },
   });
   if (!product) return null;
-  return <h1>{product.name}</h1>;
+  return (
+    <div>
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+    </div>
+  );
 };
 
 export default ProductDetailsPage;
