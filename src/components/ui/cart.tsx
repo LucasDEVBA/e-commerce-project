2b40564b475d22258f5f286b4665d2a31ac02af3
Lucas <lucasdevba@gmail.com>
2023-11-13 23:16:13 -0300

    feat: add createOrder server action and correction schema prisma

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -10,11 +10,22 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
+  const { data } = useSession();
+
   const { products, subTotal, total, totalDiscount } = useContext(cartContext);
 
   const handleFinishPurchaseClick = async () => {
+    // create order in database
+    if (!data?.user) {
+      // TODO: Return login page
+      return;
+    }
+    await createOrder(products, (data?.user as any).id);
+
     const checkout = await createCheckout(products);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
